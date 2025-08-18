c36313b7b900423673da579ad1fd10021f498512
FranklynSantos <franklynsantos451@gmail.com>
2025-08-18 09:36:42 -0300

    Estoque Terceira versão 0.0.3
    Aqui fiz os produtos serem adicionados ou removidos do estoque, terminando assim o código.

--- src/Estoque.java
+++ src/Estoque.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 public class Estoque {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Produto x;
-        x = new Produto();
+        Produto product;
+        product = new Produto();
 
         System.out.println("-----Replacement----- ");
         System.out.println("Enter product data:");
         System.out.print("Name: ");
-        x.name = sc.next();
+        product.name = sc.next();
         System.out.print("Price: $");
-        x.price = sc.nextDouble();
+        product.price = sc.nextDouble();
         System.out.print("Amount: ");
-        x.amount = sc.nextInt();
+        product.amount = sc.nextInt();
 
 
-        System.out.println(x);
+        System.out.println("Product Data: " + product);
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        int amount = sc.nextInt();
+        product.AddProducts(amount);
+
+        System.out.println("");
+        System.out.println("Updated data: " + product);
+
+        System.out.println("");
+        System.out.println("Enter the number of products to be removed from stock: ");
+        amount = sc.nextInt();
+        product.RemoveProducts(amount);
+        System.out.println("Updated data: " + product);
+
 
 
     }
