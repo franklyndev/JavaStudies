95a4dbe2b6f14ac886db41570dcbfb7e9cd4482a
FranklynSantos <franklynsantos451@gmail.com>
2025-09-01 16:57:07 -0300

    Herança em Java
    Conceito de herança na prática

---
+++ src/heranca/polimorfismo/BusinessAccount.java
@@ -0,0 +1,29 @@
+package heranca.polimorfismo;
+
+public class BusinessAccount extends Account{
+
+    private Double loanLimit;
+
+
+    public BusinessAccount(){
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance); // super: usado para chamar atributos da (superClasse)classe que extendemos
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit(){
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit){
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if (amount <= loanLimit){
+            balance += amount - 10.00;
+        }
+    }
+}
