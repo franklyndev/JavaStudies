393758efbd12f9bb9d464a16f819d277047463ba
FranklynSantos <franklynsantos451@gmail.com>
2025-09-09 13:51:55 -0300

    Exercício de fixação | Polimorfismo
    Como fazer polimorfismo na prática

---
+++ src/heranca/polimorfismo/employee/excercise/OutSourcedEmployee.java
@@ -0,0 +1,32 @@
+package heranca.polimorfismo.employee.excercise;
+
+public class OutSourcedEmployee extends Employee{
+
+    private Double additionalCharge;
+
+    public OutSourcedEmployee(){
+        super();
+    }
+
+    public OutSourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public Double payment(){
+        return super.payment() + additionalCharge *  1.1;
+    }
+
+    public String toString(){
+        return "PAYMENTS: \n" + name + " - $" + String.format("%.2f", payment());
+    }
+}
